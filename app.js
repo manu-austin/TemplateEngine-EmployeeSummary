63056395e76a7557a2329a0a5422ddc96c557f4b
Emmanuel Durand <shernua@gmail.com>
2020-10-04 14:04:08 -0500

    added app.js info about manager and function to add member of team

--- app.js
+++ app.js
@@ -1,35 +1,151 @@
+// classes
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const render = require("./lib/htmlRenderer");
+
+// npm packages
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
+// create variable for the team file path - join so that it goes well on any device and browser
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
+// cards
+const managerCard = require('./templates/managerhtml');
+const internCard = require('./templates/internhtml');
+const engineerCard = require('./templates/engineerhtml');
+
+// create team array manager + engineer(s) + intern(s)
+const myTeam = [];
+
+// first questions to user about the manager - name, ID, Emmail
+function promptUser() {
+
+    inquirer.prompt([{
+            type: "input",
+            name: "managerName",
+            message: "What is the manager's name?"
+        },
+        {
+            type: "input",
+            name: "managerID",
+            message: "What is the manager's ID?"
+        },
+        {
+            type: "input",
+            name: "managerEmail",
+            message: "What is the manager's Email??"
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "What is the manager's phone number?"
+        },
+    ])
+
+    .then(response => {
+        // create new object and fill info 
+        const manager = new Manager(
+            response.managerName,
+            response.managerID,
+            response.managerEmail,
+            response.officeNumber
+        );
+
+        const managerCard = managerCard(manager);
+        myTeam.push(managerCard);
+        otherTeamMember();
+    })
+
+};
+
+function otherTeamMember() {
+    inquirer.prompt([{
+            type: "list",
+            name: "memberType",
+            message: "What member type would you like to add?",
+            choices: [
+                "Intern",
+                "Engineer",
+                "No more members"
+            ]
+        }])
+        .then(response => {
+            switch (response.otherTeamMember) {
+                case "Intern":
+                    addIntern();
+                    break;
+                case "Engineer":
+                    addEngineer();
+                    break;
+                case "No more members":
+                    CreateTeam();
+                    break;
+            }
+
+        })
+};
 
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+function promptUser() {
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+    inquirer.prompt([{
+            type: "input",
+            name: "managerName",
+            message: "What is the manager's name?"
+        },
+        {
+            type: "input",
+            name: "managerID",
+            message: "What is the manager's ID?"
+        },
+        {
+            type: "input",
+            name: "managerEmail",
+            message: "What is the manager's Email??"
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "What is the manager's phone number?"
+        },
+    ])
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+    .then(response => {
+        // create new object and fill info 
+        const manager = new Manager(
+            response.managerName,
+            response.managerID,
+            response.managerEmail,
+            response.officeNumber
+        );
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+        const managerCard = managerCard(manager);
+        myTeam.push(managerCard);
+        otherTeamMember();
+    })
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+};
+
+// runs the function
+promptUser();
+
+//  {
+//     type: "list",
+//     name: "licenseList",
+//     message: "Please pick a license for this application from the list below",
+//     choices: [
+//         "Apache License 2.0",
+//         "BSD 3-Clause 'New' or 'Revised' license",
+//         "BSD 2-Clause 'Simplified' or 'FreeBSD' license",
+//         "GNU General Public License (GPL)",
+//         "GNU Library or 'Lesser' General Public License (LGPL)",
+//         "MIT license",
+//         "Mozilla Public License 2.0",
+//         "Common Development and Distribution License",
+//         "Eclipse Public License version 2.0",
+//     ]
+// },
